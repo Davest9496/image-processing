700b360f745224e03f556872f34f7c3f0803df6e
Dave Ejezie <evergreendave1@gmail.com>
2024-12-12 13:50:15 +0000

    test: create test for express server and endpoint

--- dist/spec/tests/index.spec.js
+++ dist/spec/tests/index.spec.js
@@ -1,7 +1,38 @@
 "use strict";
-// Test file for index
-describe('hello world test', () => {
-    it('should return true', () => {
-        expect(true).toBe(true);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const supertest_1 = __importDefault(require("supertest"));
+const express_1 = __importDefault(require("express"));
+const router_1 = require("../../src/routes/router");
+const logger_1 = require("../../src/utilities/logger");
+describe('Express server', () => {
+    let app;
+    // Set up a server instance before each test
+    beforeEach(() => {
+        app = (0, express_1.default)();
+        app.use(logger_1.logger);
+        app.use('/api', router_1.routes);
+    });
+    // API endpoint/Routes is working
+    describe('API Endpoints', () => {
+        it('should respond to /api and return 200', async () => {
+            const response = await (0, supertest_1.default)(app)
+                .get('/api')
+                .expect('Content-Type', /text\/html/)
+                .expect(200);
+            expect(response.text).toBe('Hello, TypeScript from Routes!');
+        });
+        it('should return 400 if query parameters are missing', async () => {
+            const response = await (0, supertest_1.default)(app)
+                .get('/api/images')
+                .expect('Content-Type', /json/)
+                .expect(400);
+            expect(response.body).toEqual({
+                error: 'Missing required query parameters',
+                required: ['width', 'height', 'filename'],
+            });
+        });
     });
 });
--- spec/tests/index.spec.ts
+++ spec/tests/index.spec.ts
@@ -1,7 +1,42 @@
-// Test file for index
+import request from 'supertest';
+import express, { Application } from 'express';
+import { routes } from '../../src/routes/router';
+import { logger } from '../../src/utilities/logger';
 
-describe('hello world test', () => {
-  it('should return true', () => {
-    expect(true).toBe(true);
+describe('Express server', () => {
+  let app: Application;
+
+  // Set up a server instance before each test
+  beforeEach(() => {
+    app = express();
+    app.use(logger);
+    app.use('/api', routes);
+  });
+
+  // API endpoint/Routes is working
+  describe('API Endpoints', () => {
+    it('should respond to /api and return 200', async () => {
+      const response: request.Response = await request(app)
+        .get('/api')
+        .expect('Content-Type', /text\/html/)
+        .expect(200);
+
+      expect(response.text).toBe('Hello, TypeScript from Routes!');
+    });
+
+    it('should return 400 if query parameters are missing', async () => {
+      const response: request.Response = await request(app)
+        .get('/api/images')
+        .expect('Content-Type', /json/)
+        .expect(400);
+
+      expect(response.body).toEqual({
+        error: 'Missing required query parameters',
+        required: ['width', 'height', 'filename'],
+      });
+    });
   });
 });
+
+
+
